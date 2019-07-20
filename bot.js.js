6c1e6f635d28425a4a8f9016787b81f7adf8486d
kingta001 <53026135+kingta001@users.noreply.github.com>
2019-07-20 15:52:45 -0700

    Add files via upload

---
+++ bot.js.js
@@ -0,0 +1,44 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const xkiller = member.guild.channels.find("name", "الشات ");
+     xkiller.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
